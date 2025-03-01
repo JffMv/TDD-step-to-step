8283108fbe281aa46b6c4bee388a81ac27dd01eb
JffMv <90947296+JffMv@users.noreply.github.com>
2025-03-01 04:10:35 +0000

    add test and implement code for pass this test

--- src/main/java/FizzBuzz.java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public static String fizzbuzz(int n) {
-        return String.valueOf(n);
+        String value = isMultiply3(n)+isMultiply5(n);
+        return value.equals("")? String.valueOf(n):value;
+    }
+    
+    private static String isMultiply5(int n){
+        return n%5==0 ? "Buzz": "";
+    }
+    private static String isMultiply3(int n){
+        return n%3==0 ? "Fizz": "";
     }
 }
