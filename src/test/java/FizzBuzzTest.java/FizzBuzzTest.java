04eab481b950f803630e39c726ff1715f871a034
JffMv <90947296+JffMv@users.noreply.github.com>
2025-03-01 04:15:05 +0000

    add test

--- src/test/java/FizzBuzzTest.java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java/FizzBuzzTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
@@ -9,15 +9,19 @@ class FizzBuzzTest {
     @Test
     void testFizzBuzzMultiplo3() {
     assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+    assertEquals("Fizz", FizzBuzz.fizzbuzz(9));
     }
 
     @Test
     void testFizzBuzzMultiplo5() {
     assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+    assertEquals("Buzz", FizzBuzz.fizzbuzz(100));
     }
 
     @Test
     void testFizzBuzzMultiplo3y5() {
     assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+    assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+    assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(90));
     }
 }
